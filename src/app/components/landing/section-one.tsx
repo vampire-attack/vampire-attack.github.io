2098ef2da5c8a9d8d1e6a0f377ed297c4f161dd3
ehsan <ehsan.saradar@gmail.com>
2024-07-02 12:09:12 +0330

    Init

---
+++ src/app/components/landing/section-one.tsx
@@ -0,0 +1,57 @@
+"use client"
+
+import { useEffect } from "react";
+import { landingData } from "@/app/copy/landing";
+
+export default function SectionOne() {
+    return (
+        <>
+            <div className="row mt-high mb-high position-relative">
+                <div className="col-xl-5 position-relative mt-high" id="main-sec-2">
+                    <img src="./img/bg-what.png" className="bg-what-main" />
+                    <div className="floating-image position-relative el-1-main">
+                        <img src="./img/el-3.png" className="el-1 " alt="What it is" />
+                        <img src="./img/vampireTooth.png" className="el-tooth appear2" alt="What it is" />
+                    </div>
+                    <div className="floating-image position-relative el-3-main">
+                        <img src="./img/el-2.png" className="el-2" alt="What it is" />
+                        <img src="./img/vampireTooth.png" className="el-tooth-2 appear2" alt="What it is" />
+                    </div>
+                    <div className="floating-image position-relative el-2-main">
+                        <img src="./img/el-1.png" className="el-3" alt="What it is" />
+                        <img src="./img/vampireTooth.png" className="el-tooth-3 appear2" alt="What it is" />
+                    </div>
+
+                    <div className="floating-image position-relative el-4-main">
+                        <img src="./img/el-4.png" className="el-4" alt="What it is" />
+                        <img src="./img/vampireTooth.png" className="el-tooth-4 appear2" alt="What it is" />
+                    </div>
+                </div>
+                <div className="col-xl-7 mt-high" style={{ ["paddingInline" as any]: "2rem" }}>
+                    {landingData.home.segmentOne.map((item, index) =>
+                    (
+                        <div className="mt-5" key={index}>
+                            <h3 className="monserrat-bold text-large text-uppercase fade-effect"
+                                dangerouslySetInnerHTML={{
+                                    __html: typeof item.title === 'string'
+                                        ? item.title
+                                        : ""
+                                }}
+                                fade-time="2" fade-direction="left"></h3>
+                            <p className="text-d4 text-medium fauna-one w-90 mt-4 fade-effect"
+                                dangerouslySetInnerHTML={{
+                                    __html: typeof item.description === 'string'
+                                        ? item.description
+                                        : ""
+                                }} fade-time="2" fade-direction="left">
+                            </p>
+                        </div>
+                    )
+
+                    )}
+                    
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ src/app/copy/header.ts
@@ -0,0 +1,9 @@
+export const headerData = {
+    "title": "unleash <br> your fury",
+    "subtitle": "Attack the top DeFi protocols and reclaim your power.",
+    "button": {
+        "name": "MIGRATE LIQUIDITY NOW",
+        "url": "#",
+        "target": "_blank"
+    }
+}
